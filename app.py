46096fedada7ca190d26f833b682214398a01f02
miketsi185 <michael@ergotherapygroup.com.au>
2026-06-11 20:57:55 +1000

    Update app.py

--- app.py
+++ app.py
@@ -69,10 +69,10 @@ def handle_google_oauth():
 
     if "code" in query_params:
         flow = get_google_flow()
-        def google_login_button():
-    flow = get_google_flow()
+def google_login_button():
+        flow = get_google_flow()
 
-    code_verifier = st.secrets["GOOGLE_CODE_VERIFIER"]
+        code_verifier = st.secrets["GOOGLE_CODE_VERIFIER"]
 
     auth_url, _ = flow.authorization_url(
         access_type="offline",
