e05ce0b7c146773f07647eee905537a20e48aa8a
miketsi185 <michael@ergotherapygroup.com.au>
2026-06-11 21:05:21 +1000

    Update app.py

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ def google_login_button():
 
         request = {
             "startDate": "2025-01-01",
-            "endDate": date.today().isoformat(),"
+            ""endDate": "2026-06-11"
             "dimensions": ["query"],
             "rowLimit": 20,
     }
