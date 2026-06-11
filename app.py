977eb18dd61c643d91ef447db9b10c85a0741d20
miketsi185 <michael@ergotherapygroup.com.au>
2026-06-11 20:51:52 +1000

    Update app.py

--- app.py
+++ app.py
@@ -128,7 +128,7 @@ def get_ga4_traffic(credentials):
         for row in rows
     ])
     
- def google_login_button():
+def google_login_button():
     flow = get_google_flow()
 
     code_verifier = secrets.token_urlsafe(64)
